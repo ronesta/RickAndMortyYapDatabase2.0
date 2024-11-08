994e8ecdcf223ab853250259363077621076a1b8
ronesta <gvozzzd123@gmail.com>
2024-11-08 19:22:30 +0300

    Initial commit

---
+++ RickAndMortyYapDatabase2.0/RickAndMortyYapDatabase2.0/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RickAndMortyYapDatabase2.0
+//
+//  Created by Ибрагим Габибли on 08.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
