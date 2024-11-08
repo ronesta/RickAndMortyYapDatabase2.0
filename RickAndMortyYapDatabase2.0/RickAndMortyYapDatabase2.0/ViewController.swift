f3b744dd53a497293c4a6e76e62ec045eaa59345
ronesta <gvozzzd123@gmail.com>
2024-11-08 21:52:04 +0300

    Create and setup ViewController and TableView

--- RickAndMortyYapDatabase2.0/RickAndMortyYapDatabase2.0/ViewController.swift
+++ RickAndMortyYapDatabase2.0/RickAndMortyYapDatabase2.0/ViewController.swift
@@ -6,13 +6,72 @@
 //
 
 import UIKit
-import YapDatabase
+import SnapKit
 
 class ViewController: UIViewController {
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+
+    var characters = [Character]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
+        setupNavigationBar()
+        setupViews()
+        getCharacters()
+    }
+
+    private func setupNavigationBar() {
+        title = "Characters"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.backgroundColor = .white
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CharacterTableViewCell.self,
+                           forCellReuseIdentifier: CharacterTableViewCell.id)
+
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    private func getCharacters() {
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: CharacterTableViewCell.id,
+            for: indexPath) as? CharacterTableViewCell else {
+            return UITableViewCell()
+        }
+
+        let character = characters[indexPath.row]
+
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        128
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
